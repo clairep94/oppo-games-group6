db6f305c6de028dd02af2f657366da53bff45df4
Mchana <133041100+Mchana@users.noreply.github.com>
2023-12-12 12:58:41 +0000

    initial placeholder logic

---
+++ api/lib/game_logic/placeholder_game.js
@@ -0,0 +1,8 @@
+//imports the state of the board i.e where all the pieces are, the game state such as player turn, game won
+//and action, that is any actions to be taken on the game
+const handleaction = (gamestate, boardstate, action) => {
+    return {gamestate:gamestate, boardstate:boardstate} //does nothing but return these variables 
+
+}
+
+module.exports = handleaction;
---
+++ api/spec/lib/game_logic/placeholder_game.spec.js
@@ -0,0 +1,7 @@
+const handlelogic = require("../../../lib/game_logic/placeholder_game");
+
+describe("handleLogic", () => {
+    test("tests gamestate and boardstate remain unchanged", () => {
+        const result = handlelogic
+    })
+})
