6986d87385136d2be7e3b2701ac6ec4ffc3147bb
Perran Thomas <perran.thomas@icloud.com>
2023-12-12 15:36:20 +0000

    Modified variable names and autoformatted code

--- api/lib/game_logic/placeholder_game.js
+++ api/lib/game_logic/placeholder_game.js
@@ -1,8 +1,8 @@
-//imports the state of the board i.e where all the pieces are, the game state such as player turn, game won
+//imports the state of the board i.e where all the pieces are,
+//  the game state such as player turn, game won
 //and action, that is any actions to be taken on the game
-const handleaction = (gamestate, boardstate, action) => {
-    return {gamestate:gamestate, boardstate:boardstate} //does nothing but return these variables 
+const handleAction = (gameState, boardState, action) => {
+  return { gameState: gameState, boardState: boardState }; //does nothing but return these variables
+};
 
-}
-
-module.exports = handleaction;
+module.exports = handleAction;
--- api/spec/lib/game_logic/placeholder_game.spec.js
+++ api/spec/lib/game_logic/placeholder_game.spec.js
@@ -1,10 +1,15 @@
-const handlelogic = require("../../../lib/game_logic/placeholder_game");
+const handleAction = require("../../../lib/game_logic/placeholder_game");
 
-describe("handleLogic", () => {
-    test("tests gamestate and boardstate remain unchanged", () => {
-        const result = handlelogic ("my game state", {number_of_cubes:64}, {actor:"id of actor", verb:"passively observe"})
-        expect (result).toEqual(
-            {gamestate:"my game state", boardstate:{number_of_cubes:64},}
-        ) 
-    })
-})
+describe("handleAction", () => {
+  test("tests gamestate and boardstate remain unchanged", () => {
+    const result = handleAction(
+      "my game state",
+      { numberOfCubes: 64 },
+      { actor: "id of actor", verb: "passively observe" },
+    );
+    expect(result).toEqual({
+      gameState: "my game state",
+      boardState: { numberOfCubes: 64 },
+    });
+  });
+});
