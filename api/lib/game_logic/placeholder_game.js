6b9ad30c45ad741ebc4809434565e4c4d81084b3
Perran Thomas <perran.thomas@icloud.com>
2023-12-12 17:46:36 +0000

    Refactoring step 1: give each 'verb' its own handler function

--- api/lib/game_logic/placeholder_game.js
+++ api/lib/game_logic/placeholder_game.js
@@ -3,6 +3,8 @@
 //and action, that is any actions to be taken on the game
 const handleAction = (gameState, boardState, action) => {
   if (action.verb === "passively observe") {
+    const verbFunction = passivelyObserve;
+    gameState = verbFunction(gameState, boardState, action);
     return {
       gameState: gameState,
       boardState: boardState,
@@ -10,17 +12,40 @@ const handleAction = (gameState, boardState, action) => {
     //return { gameState: gameState, boardState: boardState };
     //does nothing but return these variables
   } else if (action.verb === "punch cube") {
-    boardState.numberOfCubes = boardState.numberOfCubes - 1;
+    const verbFunction = punchCube;
+    gameState = verbFunction(gameState, boardState, action);
+    //boardState.numberOfCubes = boardState.numberOfCubes - 1;
     return {
       gameState: gameState,
       boardState: boardState,
     };
   } else if (action.verb === "give up and go home") {
+    const verbFunction = giveUpAndGoHome;
+    gameState = verbFunction(gameState, boardState, action);
     return {
-      gameState: "game over",
+      gameState: gameState,
       boardState: boardState,
     };
   }
 };
+
+const passivelyObserve = (gameState, boardState, action) => {
+  return gameState;
+  // ^ Maybe this (having to return gameState) can be avoided by wrapping
+  // the gameState in an object? i.e.
+  // old: gameState: "something"; new: gameState: { id: "something" }
+  // Another advantage of this method is that it lets you encode additional
+  // details within the game state; for instance, the ending timestamp of a game
+  // (only known once the game has ended) could be stored in the "game over" state.
+};
+
+const punchCube = (gameState, boardState, action) => {
+  boardState.numberOfCubes = boardState.numberOfCubes - 1;
+  return gameState;
+}
+
+const giveUpAndGoHome = (gameState, boardState, action) => {
+  return "game over";
+}
 
 module.exports = handleAction;
