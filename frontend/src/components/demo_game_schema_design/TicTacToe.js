9f0d5dce0a3b160c422f581937de0b10187c8383
Claire Peng <128436909+clairep94@users.noreply.github.com>
2023-12-12 19:59:40 +0000

    adds more frontend logic

--- frontend/src/components/demo_game_schema_design/TicTacToe.js
+++ frontend/src/components/demo_game_schema_design/TicTacToe.js
@@ -1,44 +1,55 @@
 import React, {useState} from "react";
 import styles from './TicTacToe.module.css'
 
+// THE BELOW IS JUST AN EXAMPLE
+// Ideally we would fetch the instance of the game from the BE with the game._id 
+
 // ======== SINGLE BUTTON ===========//
-//ideally we would fetch the instance of the game
-//and load {row, col, tictactoeGame}
-const TicTacToeButton = ({ row, col, turnNum, setTurnNum }) => {
+// ideally load {tictactoeGame}
+const TicTacToeButton = ({ row, col, turnNum, setTurnNum, XPlacements }) => {
     const [buttonActive, setButtonActive] = useState(true);
     const [space, setSpace] = useState(" ");
-  
+
     const handleCoordinateSelection = async (event) => {
       // skipping FE/BE connection for now:
       // this button would actually write to the DB for this specific TicTacToe game
-      console.log(`Selected: Row ${row}, Col ${col}`);
-      setButtonActive(false);
-  
-      // ideally there is also logic that checks whether the user is X or O, but here we'll only use X
-      if (turnNum % 2 === 0) {
-        setSpace("X");
-        setTurnNum(turnNum + 1);
-      } else {
-        setSpace("O");
-        setTurnNum(turnNum + 1);
-      }
+        console.log(`Selected: Row ${row}, Col ${col}`);
+        setButtonActive(false);
+
+      // ideally there is also logic that checks whether the user is X or O
+        if (turnNum % 2 === 0) {
+            setSpace("X");
+            setTurnNum(turnNum + 1);
+
+            //temp logic
+            // setXPlacements(XPlacements.append(`${row}${col}`))
+            // function for checking for wins which includes set-winner
+
+        } else {
+            setSpace("O");
+            setTurnNum(turnNum + 1);
+
+            //temp logic
+            // setYPlacements(YPlacements.append(`${row}${col}`))
+            // function for checking for wins which includes set-winner
+        }
     };
-  
+
     return (
-      <>
-        <button
-          aria-label={`${row}${col} button`}
-          onClick={(event) => handleCoordinateSelection(event)}
-          className={buttonActive ? "active-ttt-space" : "inactive-ttt-space"}
-          disabled={!buttonActive}
-          style={{ width: "100px", height: "100px" }}
-        >
-          <span style={{ display: "inline-block", minWidth: "100%" }}>{space}</span>
-        </button>
-      </>
-    );
-  };
-  
+        <>
+            <button
+            aria-label={`${row}${col} button`}
+            onClick={(event) => handleCoordinateSelection(event)}
+            className={buttonActive ? "active-ttt-space" : "inactive-ttt-space"}
+            disabled={!buttonActive}
+            style={{ width: "100px", height: "100px" }}
+            >
+            <span style={{ display: "inline-block", minWidth: "100%" }}>{space}</span>
+            </button>
+        </>
+        );
+    };
+    
 
 // ======== BOARD ===========//
 //ideally we would fetch the instance of the game
@@ -46,18 +57,47 @@ const TicTacToeButton = ({ row, col, turnNum, setTurnNum }) => {
 const TicTacToe = ({ navigate }) => {
     // would have states such as gameFinished, etc.
     const [turnNum, setTurnNum] = useState(0)
+    const [winner, setWinner] = useState(null)
 
+    // ========== GAME LOGIC ============
     // ideally this is loaded from the tictactoeGame
     const boardMatrix = {
         A : [1, 2, 3],
         B : [1, 2, 3],
         C : [1, 2, 3]
     }
-
-    // temporary:
+    // temporary, will be replaced with above:
     const rowA = [1, 2, 3]
     const rowB = [1, 2, 3]
     const rowC = [1, 2, 3]
+    
+    //winning combinations -- will be stored in the BE:
+    
+
+    //move to backend
+    const winningCombinations = [
+        ["A1", "A2", "A3"],
+        ["B1", "B2", "B3"],
+        ["C1", "C2", "C3"],
+
+        ["A1", "B1", "C1"],
+        ["A2", "B2", "C2"],
+        ["A3", "B3", "C3"],
+
+        ["A1", "B2", "C3"],
+        ["A3", "B2", "C1"]
+    ]
+
+    //
+    const checkForWinner = () => {
+        console.log("Check for winner function")
+        //if .....
+            // setWinner("O")
+        //else if...
+            // setWinner("X")
+    }
+
+
 
     // ideally this component would continuously access the DB to watch
     // for board changes & re-render when the board is changed
@@ -110,6 +150,8 @@ const TicTacToe = ({ navigate }) => {
                 <br/>
             </div>
         </div>
+
+        {winner && <p aria-label="Winner Announcement">{winner} wins!</p>}
 
 
         </>
