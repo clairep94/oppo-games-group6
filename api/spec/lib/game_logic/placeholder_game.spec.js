1050885248a92268c2437dfa2f6eb941d0f7b7cd
Mchana <133041100+Mchana@users.noreply.github.com>
2023-12-12 13:13:45 +0000

    test worky

--- api/spec/lib/game_logic/placeholder_game.spec.js
+++ api/spec/lib/game_logic/placeholder_game.spec.js
@@ -2,6 +2,9 @@ const handlelogic = require("../../../lib/game_logic/placeholder_game");
 
 describe("handleLogic", () => {
     test("tests gamestate and boardstate remain unchanged", () => {
-        const result = handlelogic
+        const result = handlelogic ("my game state", {number_of_cubes:64}, {actor:"id of actor", verb:"passively observe"})
+        expect (result).toEqual(
+            {gamestate:"my game state", boardstate:{number_of_cubes:64},}
+        ) 
     })
 })
