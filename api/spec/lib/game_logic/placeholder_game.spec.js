a3aa9a5578dc934a3b582749fad79966ad67b5dd
Perran Thomas <perran.thomas@icloud.com>
2023-12-12 22:26:58 +0000

    Replace "my game state" with "a game state"

--- api/spec/lib/game_logic/placeholder_game.spec.js
+++ api/spec/lib/game_logic/placeholder_game.spec.js
@@ -3,31 +3,31 @@ const handleAction = require("../../../lib/game_logic/placeholder_game");
 describe("handleAction", () => {
   test("gameState and boardState remain unchanged", () => {
     const result = handleAction({
-      progressState: "my game state",
+      progressState: "a game state",
       boardState: { numberOfCubes: 64 } },
       { actor: "id of actor", verb: "passively observe" },
     );
     expect(result).toEqual({
-      progressState: "my game state",
+      progressState: "a game state",
       boardState: { numberOfCubes: 64 },
     });
   });
 
   test("gameState remains unchanged and boardState cube count decreases by 1", () => {
     const result = handleAction({
-      progressState: "my game state",
+      progressState: "a game state",
       boardState: { numberOfCubes: 64 } },
       { actor: "id of actor", verb: "punch cube" },
     );
     expect(result).toEqual({
-      progressState: "my game state",
+      progressState: "a game state",
       boardState: { numberOfCubes: 63 },
     });
   });
 
   test("gameState changes (Game Over) and boardState does not change", () => {
     const result = handleAction({
-      progressState: "my game state",
+      progressState: "a game state",
       boardState: { numberOfCubes: 64 } },
       { actor: "id of actor", verb: "give up and go home" },
     );
@@ -39,7 +39,7 @@ describe("handleAction", () => {
 
   test("when the last cube is broken, the game is won", () => {
     const result = handleAction({
-      progressState: "my game state",
+      progressState: "a game state",
       boardState: { numberOfCubes: 1 } },
       { actor: "id of actor", verb: "punch cube" },
     );
