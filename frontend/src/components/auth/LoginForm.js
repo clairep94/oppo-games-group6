3931b6e2221bf3b66c4abd61143edf49a92076fc
Claire Peng <128436909+clairep94@users.noreply.github.com>
2023-12-12 12:53:11 +0000

    adds error message to login component

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
+// import { useLocation } from 'react-router-dom'; // use this for login-popup when timed-out
+import styles from './LoginForm.module.css'
 
 const LogInForm = ({ navigate }) => {
+
+  // =========== STATE VARIABLES ==========================
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  // const location = useLocation(); // use this for login-popup when timed-out
 
+
+
+  // ============ FORM SUBMISSION FOR LOGIN ====================
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    // Send POST request to '/tokens' endpoint
     let response = await fetch( '/tokens', {
       method: 'post',
       headers: {
@@ -15,17 +24,34 @@ const LogInForm = ({ navigate }) => {
       body: JSON.stringify({ email: email, password: password })
     })
 
-    if(response.status !== 201) {
-      console.log("yay")
-      navigate('/login')
-    } else {
-      console.log("oop")
+    // Checking the response status
+    if(response.status === 401){ // wrong password
+      setError("Enter a valid email or password")
+    } else if (response.status !== 201) { // if error code is not 401 or 201, show server error
+      setError("Server error, please try again later")
+    } else { // login successful
       let data = await response.json()
       window.localStorage.setItem("token", data.token)
-      navigate('/lobby');
+      
+
+      // TEMP: No timeout login popup:
+      navigate('/lobby')
+
+      // FOR FUTURE USE IF HAVING TIMEOUT LOGIN POPUP:
+      // // Check the current location and navigate accordingly
+      // if (location.pathname === '/') {
+      //   navigate('/timeline');
+      // } else {
+      //   // 
+      // }
+      // window.location.reload(); // Necessary addition so that page after successful login if logging in after timed out
     }
   }
 
+
+
+  // ------------ SUPPORTIVE FUNCTIONS: ----------------
+  // FUNCTIONS FOR CHANGING STATE VARIABLES 
   const handleEmailChange = (event) => {
     setEmail(event.target.value)
   }
@@ -34,8 +60,14 @@ const LogInForm = ({ navigate }) => {
     setPassword(event.target.value)
   }
 
+  // Enter a valid email or password
 
+
+  // ========= JSX FOR THE UI OF THE COMPONENT =====================
+  // for all styling: use className={styles.Button}
     return (
+      <>
+      <h2>Login</h2>
       <form onSubmit={handleSubmit}>
         <label for="Email" class="accessible_visuallyhidden">Email: </label>
         <input placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
@@ -46,8 +78,10 @@ const LogInForm = ({ navigate }) => {
         <label for="login-button" class="accessible_visuallyhidden">Login Button: </label>
         <input role='login-button' id='login-button' type="submit" value="Login" />
 
-
       </form>
+
+      {error && <p className={styles.errorMessage}>{error}</p>}
+      </>
     );
 }
 
