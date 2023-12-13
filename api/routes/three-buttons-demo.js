a0f862099b9b164a168c4bc687aa365c1be2c1fe
Perran Thomas <perran.thomas@icloud.com>
2023-12-13 16:28:30 +0000

    Added three endpoints to the router, using POST, GET, and PATCH methods

--- api/routes/three-buttons-demo.js
+++ api/routes/three-buttons-demo.js
@@ -2,3 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const ThreeButtonsDemoController = require("../controllers/three-buttons-game");
+
+// Hit the big reset switch on the three buttons game
+router.post("/", ThreeButtonsDemoController.Restart);
+
+// Get our mail (we won't be allowed to get other people's mail...)
+router.get("/mailbox", ThreeButtonsDemoController.CheckMail);
+
+// Interact with the game
+router.patch("/game", ThreeButtonsDemoController.DoGameAction);
+
