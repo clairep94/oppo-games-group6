521086cdc9bd51de1f2768bdfd9412b09ae2517e
Perran Thomas <perran.thomas@icloud.com>
2023-12-14 14:33:32 +0000

    Filled out three buttons game controller with placeholder methods

--- api/controllers/three-buttons-games.js
+++ api/controllers/three-buttons-games.js
@@ -3,3 +3,19 @@ const TokenGenerator = require("../lib/token_generator");
 
 // Import game logic function
 const handleGameAction = require("../lib/game-logic/three-buttons-game");
+
+const ThreeButtonsGamesController = {
+  Create: (req, res) => {
+    res.status(400).json({message: 'Not implemented yet'});
+  },
+  Index: (req, res) => {
+    res.status(400).json({message: 'Not implemented yet'});
+  },
+  FindByID: (req, res) => {
+    res.status(400).json({message: 'Not implemented yet'});
+  },
+  DoGameAction: (req, res) => {
+    res.status(400).json({message: 'Not implemented yet'});
+  },
+
+}
