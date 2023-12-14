2589897cf7de32bc6f1d9b9ab806c77dca448b16
Onuora <onuora.wright@gmail.com>
2023-12-14 11:01:40 +0000

    Added username field to sign up page in front/src/components/user/SignUpForm.js

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const SignUpForm = ({ navigate }) => {
 
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -24,6 +25,10 @@ const SignUpForm = ({ navigate }) => {
       })
   }
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+  
   const handleEmailChange = (event) => {
     setEmail(event.target.value)
   }
@@ -35,9 +40,10 @@ const SignUpForm = ({ navigate }) => {
 
     return (
       <form onSubmit={handleSubmit}>
+          <input placeholder="Username" id="username" type='text' value={ username } onChange={handleUsernameChange} />
           <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
           <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <input id='submit' type="submit" value="Submit" />
+          <input id='submit' type="submit" value="Submit" />
       </form>
     );
 }
