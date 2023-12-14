d00f8bc16eb71679a9f04849dff25071d9aa3a84
Perran Thomas <perran.thomas@icloud.com>
2023-12-14 10:51:44 +0000

    Started working on sketch for three buttons game controller

--- api/controllers/three-buttons-game.js
+++ api/controllers/three-buttons-game.js
@@ -1,7 +1,36 @@
 const ThreeButtonsGame = require("../models/three-buttons-game");
 
 const ThreeButtonsDemoController = {
+  Index: (req, res) => {
+    ThreeButtonsGame.findOne().exec((err, game) => {
+      if (err) {
+        throw err;
+      }
+      res.status(200).json({ game: game });
+    })
+  },
 
+  Restart: (req, res) => {
+    ThreeButtonsGame.deleteMany();
+    const game = new Game({
+      // data will go in here
+    });
+    game.save((err) => {
+      if (err) {
+        throw err;
+      }
+
+      res.status(201).json({ message: 'OK' });
+    });
+  },
+
+  CheckMail: (req, res) => {
+    
+  },
+
+  DoGameAction: (req, res) => {
+
+  },
 };
 
 module.exports = ThreeButtonsDemoController;
