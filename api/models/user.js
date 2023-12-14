c7b846daed175bfb387905e827c640eb2914f9cc
Onuora <onuora.wright@gmail.com>
2023-12-14 10:56:26 +0000

    Added username to database schema in api/models/user.js

--- api/models/user.js
+++ api/models/user.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 // TODO Unit testing for User Schema
 
 const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
