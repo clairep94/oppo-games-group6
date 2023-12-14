9d3e6af5d624804b4314b73a89a2ecbf423ff8d8
Perran Thomas <perran.thomas@icloud.com>
2023-12-14 15:48:53 +0000

    Updated placeholder functions to hopefully return something of the right form

--- api/controllers/three-buttons-games.js
+++ api/controllers/three-buttons-games.js
@@ -6,16 +6,48 @@ const handleGameAction = require("../lib/game-logic/three-buttons-game");
 
 const ThreeButtonsGamesController = {
   Create: (req, res) => {
-    res.status(400).json({message: 'Not implemented yet'});
+    ThreeButtonsGame
+    .find()
+    .exec((err, games) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(400).json({message: 'Not implemented yet'});
+      }
+    });
   },
   Index: (req, res) => {
-    res.status(400).json({message: 'Not implemented yet'});
+    ThreeButtonsGame
+    .find()
+    .exec((err, games) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(400).json({message: 'Not implemented yet'});
+      }
+    });
   },
   FindByID: (req, res) => {
-    res.status(400).json({message: 'Not implemented yet'});
+    ThreeButtonsGame
+    .find()
+    .exec((err, games) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(400).json({message: 'Not implemented yet'});
+      }
+    });
   },
   DoGameAction: (req, res) => {
-    res.status(400).json({message: 'Not implemented yet'});
+    ThreeButtonsGame
+    .find()
+    .exec((err, games) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(400).json({message: 'Not implemented yet'});
+      }
+    });
   },
 
-}
+};
