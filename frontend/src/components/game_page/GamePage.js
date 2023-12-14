8dd1babc955c30a698f2508f5d2967332599b82e
Perran Thomas <perran.thomas@icloud.com>
2023-12-14 13:29:42 +0000

    Set up GamePage component to always render ThreeButtonsGame

---
+++ frontend/src/components/game_page/GamePage.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import ThreeButtonsGame from "../../games/threebuttonsgame/ThreeButtonsGame";
+import { useParams } from 'react-router-dom';
+
+// Currently, GamePage is hardcoded to point to ThreeButtonsGame.
+
+// This component should eventually be accessible at /<title of game>/<game id>
+// for all game titles
+
+const GamePage = ({ navigate, gameTitle }) => {
+  // Parameter should be ID of game
+  const { gameId } = useParams();
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
+
+  // Ensure game page only available when logged in
+  useEffect(() => {
+    if (token) {
+      // TODO: figure out if anything needs to be done here
+    } else {
+      navigate("/login");  // Redirect to login screen
+    }
+  }, [token, navigate]);
+
+  // Button functions. These should be removed later once a navbar takes care of it
+  const logout = () => {
+    window.localStorage.removeItem("token");
+    setToken(null); // this should make useEffect log you out immediately
+  };
+  const backToLobby = () => {
+    navigate("/lobby");
+  };
+
+  // For now: Hardcode three buttons game
+  // TODO: Change it so it can point to any game.
+  gameTitle = "three-buttons-game"
+
+  return (
+    <>
+      {/* TODO: Replace this with a navbar component with ARIA labels etc */}
+      <div className="psuedo-navbar">
+        <button onClick={logout}>Log out</button>
+        <button onClick={backToLobby}>Back to Lobby</button>
+      </div>
+      <ThreeButtonsGame gameId={ gameId }/>
+    </>
+  );
+};
+
+export default GamePage;
