d131b577ed57cfad432df7782889c38a9b89c7dc
Perran Thomas <perran.thomas@icloud.com>
2023-12-14 13:39:06 +0000

    Fixed route so that it also includes the ID of the specific game

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -18,7 +18,7 @@ const App = () => {
           {/* <Route path='/posts'  element={<Feed navigate={ useNavigate() }/>}/> */}
           <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
           <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
-          <Route path='/three-buttons' element={<GamePage navigate={ useNavigate() } gameTitle={ "three-buttons-game" }/>}/>
+          <Route path='/three-buttons/:gameId' element={<GamePage navigate={ useNavigate() } gameTitle={ "three-buttons-game" }/>}/>
         </Routes>
     );
 }
