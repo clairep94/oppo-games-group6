d4b3148a812e3a17ffeeae48edcc6c660bc46680
Perran Thomas <perran.thomas@icloud.com>
2023-12-14 14:18:34 +0000

    Create game-logic directory with empty three-button-game.js file

--- api/controllers/three-buttons-games.js
+++ api/controllers/three-buttons-games.js
@@ -2,4 +2,4 @@ const ThreeButtonsGame = require("../models/three-buttons-game");
 const TokenGenerator = require("../lib/token_generator");
 
 // Import game logic function
-const handleGameAction = require("../lib/game-logic/three-button-game.js");
+const handleGameAction = require("../lib/game-logic/three-button-game");
