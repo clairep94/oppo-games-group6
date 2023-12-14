0decf033ea4c52ea9d2ef89883151f4964ffc646
Onuora <onuora.wright@gmail.com>
2023-12-14 16:27:17 +0000

    Updated user model to so usernames and email addresses must be unique

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -22,7 +22,7 @@ const SignUpForm = ({ navigate }) => {
     }
 
     if (!isValidPassword(password)) {
-      setError("Password must be 8 characters or more, contain at least 1 lowercase letter, 1 uppercase letter, 1 special character and 1 number.");
+      setError("Password have at least 8 characters and have at least 1 lowercase letter, 1 uppercase letter, 1 special character and 1 number.");
       return;
     } else {
       setError("");
@@ -45,7 +45,7 @@ const SignUpForm = ({ navigate }) => {
     })
       .then(response => {
         if(response.status === 201) {
-          navigate('/login')
+          navigate('/lobby')
         } else {
           navigate('/signup')
         }
