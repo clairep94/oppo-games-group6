e9942b4528af9ba2d6bbdf602e9b4e9b368e50d1
Perran Thomas <perran.thomas@icloud.com>
2023-12-14 14:11:05 +0000

    Filled out three-buttons-games router

---
+++ api/routes/three-buttons-games.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const ThreeButtonsGamesController = require("../controllers/three-buttons-game");
+
+router.get("/", ThreeButtonsGamesController.Index);
+router.get("/:id", ThreeButtonsGamesController.FindByID);
+router.post("/", ThreeButtonsGamesController.Create);
+
+// This is responsible for processing a wide variety of in-game actions
+// (Such as joining the game, making moves, resigning, etc.)
+router.put("/:id", ThreeButtonsGamesController.DoGameAction);
+
+module.exports = router;
