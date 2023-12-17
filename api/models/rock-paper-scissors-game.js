f0b0fd8a7f7703e47b03100e398cf850a967dd48
Perran Thomas <perran.thomas@icloud.com>
2023-12-17 14:14:02 +0000

    Wrote schema for rock paper scissors game

--- api/models/rock-paper-scissors-game.js
+++ api/models/rock-paper-scissors-game.js
@@ -1,27 +1,61 @@
+const mongoose = require("mongoose");
 
+const ActionSchema = new mongoose.Schema({
+  // This is used for logging purposes.
+  performedAt: Date,
+  op: String,
+  args: String, // Use JSON.stringify() to set this
+  playerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
 
-// - Schema:
-//     - **First needed for AWAITING_HOST**
-//         - `_id` auto-assigned on game creation
-//         - `title: String` with `"Rock Paper Scissors"`
-//         - `createdAt: Date` using `Date.now()`
-//         - `players: [UserId]` with `[]`
-//         - `updatedAt: Date` with `createdAt`
-//         - `actionLog: [Action]` with `[]`
-//         - `progressState: String` with `AWAITING_HOST`
-//     - **First needed for AWAITING_GAME**
-//         - `hostId: UserId` with UserId of host
-//         - `settings: Object` with default settings; properties:
-//             - `gameLength: Number` with `1`
-//         - `isReady: [Boolean]` with `[false]`
-//     - **First needed for PLAYING_GAME**
-//         - `signsThrown: [[String]]` with `[["none", "none"]]`
-//             - *This is (static) private information! While the game is in progress, don't show the other client what was picked, instead responding to client 1 with e.g. [["scissors", "rock"], ["paper", null]] if client 1 picked paper in round 2 and is waiting for client 2's pick.*
-//         - `currentRound` with 1
-//         - `scores: [Number]` with `[0, 0]`
-//     - **First needed for CONCLUDED**
-//         - `concludedAt: Date` with `Date.now()` on conclusion
-//         - `conclusionType: String` with `"normal"` or `"by-resignation"`
-//         - `playerResults: [Object]` with description of results; properties:
-//             - `outcome: String` with `"won"`, `"lost"`, or `"resigned"`
-//             - `finalScore: Number` with `0` or `1` for a best-of-1 game, up to `2` for a best-of-3 game
+const RockPaperScissorsGameSchema = new mongoose.Schema({
+  // progressState stores a code corresponding to the game object's high-level state
+  progressState: String,
+
+  // Section 1: Properties first needed for state AWAITING_HOST
+  title: String,
+  createdAt: Date,
+  updatedAt: Date,
+  players: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User',
+  },
+  actionLog: {
+    type: [ActionSchema],
+  },
+
+  // Section 2: Properties first needed for state AWAITING_GAME
+  hostId: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User',
+  },
+  settings: {
+    gameLength: Number,
+  },
+  isReady: [Boolean],
+
+  // Section 3: Properties first needed for state PLAYING_GAME
+  currentRound: Number,
+  signsThrown: [[String]], // e.g. signsThrown[1][0] is Player 1's sign in Round 2
+  scores: [Number],
+
+  // Section 4: Properties first needed for state CONCLUDED
+  concludedAt: Date,
+  conclusionType: String, // "normal" or "by-resignation"
+  playerResults: {
+    type: [{
+      outcome: String, // "won", "lost", or "resigned"
+      finalScore: Number,
+    }],
+  },
+
+});
+
+const RockPaperScissorsGame = mongoose.model(
+  "RockPaperScissorsGame", RockPaperScissorsGameSchema
+);
+
+module.exports = RockPaperScissorsGame;
