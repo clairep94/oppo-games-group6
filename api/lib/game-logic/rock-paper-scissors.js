6c21f2bf6b01eb97a2d20f5a08bc7c9b6f97b06f
Perran Thomas <perran.thomas@icloud.com>
2023-12-17 16:49:24 +0000

    Added OPS constants to game logic script

--- api/lib/game-logic/rock-paper-scissors.js
+++ api/lib/game-logic/rock-paper-scissors.js
@@ -7,6 +7,16 @@
 // function makeGameSnapshot(game, playerId) -> gameSnapshot
 // (See /template_documentation/ROCK_PAPER_SCISSORS_DESIGN.md for details)
 
+const OPS = {
+  JOIN: "join",
+  SETUP: "setup", // args: (settings)
+  READY: "ready",
+  // QUIT: "quit", // QUIT and KICK don't need to be implemented yet
+  // KICK: "kick", // args: (playerId)
+  THROW: "throw", // args: (roundNumber, handSign)
+  RESIGN: "resign",
+};
+
 const RESPONSE_CODES = {
   OK: "ok",
   INVALID: "invalid",
@@ -16,6 +26,7 @@ const RESPONSE_CODES = {
 // Placeholder "do-nothing" functions
 
 const getNewGame = () => {
+  // Refer to schema & docs for info on what this should return.
   const now = Date.now();
   const newGame = {
     title: "Rock Paper Scissors",
