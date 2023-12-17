0e20e9f496c63adc0dd26cfb8c6ce9ca4b1c1f97
Perran Thomas <perran.thomas@icloud.com>
2023-12-17 15:02:27 +0000

    Fixed typo in function name

--- api/routes/rock-paper-scissors-games.js
+++ api/routes/rock-paper-scissors-games.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const RockPaperScissorsGamesController = require("../controllers/rock-paper-scissors-games");
 
 router.get("/", RockPaperScissorsGamesController.Index);
-router.get("/:id", RockPaperScissorsGamesController.FindByID);
+router.get("/:id", RockPaperScissorsGamesController.FindById);
 router.post("/", RockPaperScissorsGamesController.Create);
 router.put("/:id/:op", RockPaperScissorsGamesController.DoGameAction);
 //router.delete("/:id", RockPaperScissorsGamesController.Delete); // No need to implement yet
