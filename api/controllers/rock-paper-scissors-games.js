02a3de646f2194d1885ed8cb0eb2dd5c2a1700f4
Perran Thomas <perran.thomas@icloud.com>
2023-12-17 15:40:53 +0000

    Wrote most of Create controller method for RPS game; still needs autojoin-as-host functionality

--- api/controllers/rock-paper-scissors-games.js
+++ api/controllers/rock-paper-scissors-games.js
@@ -11,6 +11,7 @@ const RockPaperScissorsGamesController = {
 
   Index: (req, res) => {
     const clientUserId = req.user_id;
+    const token = TokenGenerator.jsonwebtoken(clientUserId);
     RockPaperScissorsGame.find()
     // The following line uses mongoose's "field name syntax" to select specific fields for the populated documents.
     // This avoids the issue with using .populate when the child schema is updated; 
@@ -19,7 +20,6 @@ const RockPaperScissorsGamesController = {
     // DO NOT POPULATE `hostId` - the host's username will already be in the `players` array,
     // and during the AWAITING_HOST state the hostId property will be null.
     .exec((err, games) => {
-      const token = TokenGenerator.jsonwebtoken(clientUserId);
       if (err) {
         res.status(500).json({ error: err, token: token });
       } else {
@@ -32,12 +32,12 @@ const RockPaperScissorsGamesController = {
 
   FindById: (req, res) => {
     const clientUserId = req.user_id;
+    const token = TokenGenerator.jsonwebtoken(clientUserId);
     const gameId = req.params.id;
     RockPaperScissorsGame.findById(gameId)
     // See above for what this .populate function call does
     .populate('players', 'username')
     .exec((err, game) => {
-      const token = TokenGenerator.jsonwebtoken(clientUserId);
       if (err) {
         res.status(500).json({ error: err, token: token });
       } else {
@@ -52,6 +52,37 @@ const RockPaperScissorsGamesController = {
       }
     });
   },
+
+  Create: (req, res) => {
+    const clientUserId = req.user_id;
+    const token = TokenGenerator.jsonwebtoken(clientUserId);
+    // Require the `shouldJoin` property to be present in the request body.
+    const shouldJoin = req.body.shouldJoin;
+    if (shouldJoin === undefined) {
+      const msg = "You must provide a `shouldJoin` property in the request body";
+      res.status(400).json({ message: msg, token: token });
+      return;
+    }
+    // `getNewGame` also handles timestamps
+    const game = new RockPaperScissorsGame(getNewGame());
+    game.save((err, game) => {
+      if (err) {
+        res.status(500).json({ error: err, token: token });
+      } else {
+        // Depending on the value of shouldJoin, join the game (as host) or not
+        if (shouldJoin) {
+          // TODO: Write the equivalent of a PUT /:id/JOIN request
+          // (Will share some code with DoGameAction)
+        }
+        // Respond with the id of the new game.
+        res.status(201).json({ message: 'OK', token: token, gameId: game.id })
+      }
+    });
+  },
+
+  DoGameAction: (req, res) => {},
+
+  //Delete: (req, res) => {},
 
 };
 
