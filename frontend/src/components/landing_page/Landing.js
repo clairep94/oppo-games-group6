f09287056ba950f74334842f1b23070219ff737b
Ray Brown <raybrown.dev@gmail.com>
2024-01-09 11:29:28 +0000

    Basic landing page design draft one complete

--- frontend/src/components/landing_page/Landing.js
+++ frontend/src/components/landing_page/Landing.js
@@ -2,7 +2,7 @@ import landingBg from "../../assets/landing_game_console.png"
 
 const Landing = ({ navigate }) => {
 
-    const basicFont = "text-white text-lg font-extralight tracking-widest text-wrap"
+    const basicFont = "pt-3 text-white text-lg font-extralight tracking-widest text-wrap"
     const bgGradPur = "bg-gradient-to-br from-gray-900 via-purple-900 to-violet-600 "
     // RGB Colours
     //linear-gradient(to right bottom, rgb(17, 24, 39), rgb(88, 28, 135), rgb(124, 58, 237))
@@ -12,38 +12,40 @@ const Landing = ({ navigate }) => {
      {/* comment  */}
 
     <div className={bgGradPur}>
-
-    <p className="text-white">this is a sample text</p>
-
-    <div>
+    <div class="container mx-auto p-9">
+        <div class="flex justify-end">
+        <div class="text-left w-3/5 pr-4">
+            
+        <div className="flex flex-col h-dvh justify-center">
         <h3 className="text-xl text-white font-semibold tracking-wider">ONLINE MULTIPLAYER GAMING</h3>
-        <h1 className="text-6xl text-white font-extrabold">OPPO GAMES</h1>
+        <h1 className="pt-3 text-8xl text-white font-extrabold">OPPO GAMES</h1>
         <h2 className={basicFont}>Welcome to Oppo Games. An online platform to participate in retro, multiplayer games in live time. Join a game, chat with your opponent and have fun!</h2>
         
-        <button>
-            <div>
-                <img src="#"></img>
-                <p>TIC TAC TOE</p>
-            </div>
+        <div className="pt-6">
+        <button className="w-2/5 bg-pink-500 text-xl text-white font-semibold rounded-lg py-2 px-4 hover:bg-pink-600 focus:outline-none focus:shadow-outline-pink active:bg-pink-700">
+        START PLAYING 
         </button>
+        </div>
 
-        <button>
-            <div>
-                <img src="#"></img>
-                <p>BATTLESHIPS</p>
             </div>
-        </button>
-    
+        </div>
+
+        <div class="w-2/5 min-h-screen flex flex-col justify-center">
+            <img 
+                src={landingBg} 
+                alt="game console" 
+                width="700px"
+                ></img>
+        </div>
+        </div>
+    </div>
     </div>
 
-    <img src={landingBg} 
-    alt="game console" 
-    ></img>
-
-    </div>
-    
     </>
     );
 };
 
 export default Landing;
+
+
+
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        "game-purple": "#140050"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
-
