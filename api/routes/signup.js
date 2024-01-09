7d78a86b466721e9cd0d291f80392daadd45f2be
Ray Brown <raybrown.dev@gmail.com>
2024-01-09 16:32:55 +0000

    removed the pink color from styling

--- api/routes/signup.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const UsersController = require("../controllers/users");
-
-// This is the only non-authentication-req'd path for the Users controller.
-router.post("/", UsersController.Create);
-
-module.exports = router;
