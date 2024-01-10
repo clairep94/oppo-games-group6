96d7358e7715235bb697b623e48528f2eabdcfba
Mchana <133041100+Mchana@users.noreply.github.com>
2024-01-10 15:00:27 +0000

    index logic

--- frontend/src/components/messages/MessagePage.js
+++ frontend/src/components/messages/MessagePage.js
@@ -1,8 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import io from "socket.io-client";
+//import MessagesController from '../../../../api/controllers/message';
+
 
 const MessagePage = ({navigate}) => {
-  return (
-  <><h2>Messages</h2></>
+  //const [messages, setmessages] = useState([])
+
+  return  (
+    <div className = "Messenger">
+        <div classname = "MessageLog" placeholder = "message history here"></div>
+        <div classname = "MessageInput" placeholder= "write message here"></div>
+        <button classname= "SubmitButton"><h2>submit</h2></button>
+
+    </div>
   )
 }
 
