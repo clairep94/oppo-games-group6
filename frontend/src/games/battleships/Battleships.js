49b260423f459c0a62630699275ea4d015e4d2c9
Perran Thomas <perran.thomas@icloud.com>
2024-01-10 17:38:01 +0000

    Completed battleships backend and did setup in preparation for frontend

---
+++ frontend/src/games/battleships/Battleships.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import getSessionUserID from "../../utility/getSessionUserID";
+
+const Battleships = ({ gameId }) => {
+  return (
+    <div className="rock-paper-scissors">
+      <h2>Battleships</h2>
+    </div>
+  );
+};
+
+
+export default Battleships;
